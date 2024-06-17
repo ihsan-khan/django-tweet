8e40f7f0903ceb6c51b4a53f7694dc3db56923e5
unknown <ehsankhan0577@gmail.com>
2024-06-17 17:49:18 +0500

    tailwind instalation and configuration

--- DjangoTweet/DjangoTweet/settings.py
+++ DjangoTweet/DjangoTweet/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tweet',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -129,5 +131,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 

0f44f8f10bd18d3c5a6f3d0990c730dbf1bc7c8a
unknown <ehsankhan0577@gmail.com>
2024-06-17 23:29:24 +0500

    navbar setup

--- DjangoTweet/DjangoTweet/settings.py
+++ DjangoTweet/DjangoTweet/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'tweet',
     'tailwind',
     'theme',
+    'django_browser_reload'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'DjangoTweet.urls'
